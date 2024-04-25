880faccec3b520bf6b87934a771c1b17d9f592ca
Andy Nieves <andinieves151720@gmail.com>
2024-04-25 23:14:40 +0800

    Initial commit

---
+++ array-object-challenge.js
@@ -0,0 +1,60 @@
+const quiz = [
+    {
+        "subject": "Sport",
+        "quiz": [
+            {
+                "question": "Which one is correct team name in NBA?",
+                "options": [
+                    "New York Bulls",
+                    "Los Angeles Kings",
+                    "Golden State Warriros",
+                    "Huston Rocket"
+                ],
+                "answer": "Huston Rocket"
+            }
+        ]
+    },
+    {
+        "subject": "Math",
+        "quiz": [
+            {
+                "question": "5 + 7 = ?",
+                "options": [
+                    "10",
+                    "11",
+                    "12",
+                    "13"
+                ],
+                "answer": "12"
+            },
+            {
+                "question": "12 - 8 = ?",
+                "options": [
+                    "1",
+                    "2",
+                    "3",
+                    "4"
+                ],
+                "answer": "4"
+            }
+        ]
+    }
+
+]
+
+/* 
+    Challenge: 30 mins
+    Display all the subject, question and corresponding answers with the following format
+    e.g.
+    Subject:  Sport
+    Question 1:  Which one is correct team name in NBA?
+    Answer:  Huston Rocket
+    -----------------------
+    Subject:  Math
+    Question 1:  5 + 7 = ?
+    Answer:  12
+    Question 2:  12 - 8 = ?
+    Answer:  4
+*/
+
+// Write your answer here
---
+++ number-challenge.js
@@ -0,0 +1,6 @@
+/*
+    1 Create a function that will return the sum 3 numbers
+    2 Create a function that will return the difference of the 2 numbers
+    3 Create a function that will return the product of the 2 numbers
+    4 Create a function that will return the average of the 2 numbers
+*/
